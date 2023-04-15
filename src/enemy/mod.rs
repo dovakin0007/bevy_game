2e25f836ee2ff58b4548739904176e407fe64568
dovakin0007 <dovakin0007@gmail.com>
2023-04-15 08:56:45 +0530

    Added sets to execute player movement before confinement

--- src/enemy/mod.rs
+++ src/enemy/mod.rs
@@ -12,14 +12,27 @@ pub const ENEMY_SPEED:f32 = 200.0;
 pub const ENEMY_SIZE:f32 = 64.0; // this is the enemy sprite size
 pub const ENEMY_SPAWN_TIME: f32 = 7.0;
 
+#[derive(SystemSet, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum EnemySystemSet{
+    Movement,
+    Confinement,
+}
+
 pub struct EnemyPlugin;
 
 impl Plugin for EnemyPlugin {
     fn build(&self, app: &mut App){
         app.init_resource::<EnemySpawnTimer>()
+            .configure_set(EnemySystemSet::Movement.before(EnemySystemSet::Confinement))
             .add_startup_system(spawn_enemy)
-            .add_system(enemy_movement)
-            .add_system(confine_enemy_movement)
+            // .add_system(enemy_movement.before(confine_enemy_movement))
+            // .add_system(confine_enemy_movement)'
+            // .add_systems((
+            //     enemy_movement.before(confine_enemy_movement),
+            //     confine_enemy_movement
+            // ))
+            .add_system(enemy_movement.in_set(EnemySystemSet::Movement))
+            .add_system(confine_enemy_movement.in_set(EnemySystemSet::Confinement))
             .add_system(update_enemy_movement)
             .add_system(start_enemy_spawn_timer)
             .add_system(start_enemy_spawn_time);
--- src/player/mod.rs
+++ src/player/mod.rs
@@ -10,13 +10,30 @@ pub const PLAYER_SIZE:f32 = 64.0;
 pub const ENEMY_SIZE:f32 = 64.0;
 pub const STAR_SIZE: f32 = 30.0;
 
+#[derive(SystemSet, Debug, PartialEq, Eq, Hash, Clone)]
+pub struct MovementSystemSet;
+
+#[derive(SystemSet, Debug, PartialEq, Eq, Hash, Clone)]
+pub struct ConfineSystemSet;
+
+
+
+
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin{
     fn build(&self, app: &mut App){
-        app.add_startup_system(spawn_player)
-        .add_system(player_movments)
-        .add_system(confine_player_movement)
+        app
+        .configure_set(MovementSystemSet.before(ConfineSystemSet))
+        .add_startup_system(spawn_player)
+        // .add_system(player_movments.before(confine_player_movement))
+        // .add_system(confine_player_movement)
+        // .add_systems((
+        //     player_movments.before(confine_player_movement),
+        //     confine_player_movement
+        // )).chain()
+        .add_system(player_movments.in_set(MovementSystemSet))
+        .add_system(confine_player_movement.in_set(ConfineSystemSet))
         .add_system(enemy_hit_player)
         .add_system(player_hit_star);
     }
